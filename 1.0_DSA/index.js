7de2d184fda37ff6ef2ee65c303629c56665ffda
creator <aicreator820@gmail.com>
2024-04-17 11:48:49 +0530

    problem solved

--- 1.0_DSA/index.js
+++ 1.0_DSA/index.js
@@ -1,3 +1,5 @@
+/////////////////////////////// the first recurring elements /////////////////////////
+
 const arr = [2, 1, 3, 1, 3, 5, 1, 5, 2, 3, 5, 1, 2, 4];
 const arr1 = [];
 for (let i = 0; i < arr.length; i++) {
