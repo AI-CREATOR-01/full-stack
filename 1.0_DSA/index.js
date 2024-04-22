b575111a5c2987f742b54bc97339f02b43d35c10
creator <aicreator820@gmail.com>
2024-04-22 16:14:01 +0530

    content updated

--- 1.0_DSA/index.js
+++ 1.0_DSA/index.js
@@ -1,12 +1,17 @@
 /////////////////////////////// the first recurring elements /////////////////////////
 
-const arr = [2, 1, 3, 1, 3, 5, 1, 5, 2, 3, 5, 1, 2, 4];
+const arr = [1, 2, 2, 3, 4];
 const arr1 = [];
+let reOccur;
 for (let i = 0; i < arr.length; i++) {
   if (arr1.includes(arr[i])) {
+    reOccur = arr[i];
     console.log(`the first reocccuring ele is ${arr[i]}`);
     break;
   } else {
     arr1.push(arr[i]);
   }
+}
+if (!reOccur) {
+  console.log("there is no reoccuring element");
 }
